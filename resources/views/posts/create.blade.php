4b2466dbe6c476939db9991c09fe16e284d423c4
Artsiom.Burkun <fartunastar_artem@mail.ru>
2017-04-01 02:13:10 +0300

    adding TinyMCE

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -7,12 +7,29 @@
 @section('stylesheet')
     {!! Html::style('/css/parsley.css') !!}
     {!! Html::style('/css/select2.min.css') !!}
+
 @endsection
 
 
 @section('scripts')
     <script src="/js/parsley.min.js"></script>
     <script src="/js/select2.min.js"></script>
+    <script src="//cloud.tinymce.com/stable/tinymce.min.js"></script>
+
+    <script>
+        tinymce.init({
+            selector: 'textarea',
+            height: 500,
+            menubar: false,
+            plugins: [
+                'advlist autolink lists link image charmap print preview anchor',
+                'searchreplace visualblocks code fullscreen',
+                'insertdatetime media table contextmenu paste code'
+            ],
+            toolbar: 'undo redo | insert | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
+            content_css: '//www.tinymce.com/css/codepen.min.css'
+        });
+    </script>
 
     <script type="text/javascript">
         $(".multiple").select2();
--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -13,6 +13,23 @@
 @section('scripts')
     <script src="/js/parsley.min.js"></script>
     <script src="/js/select2.min.js"></script>
+    <script src="//cloud.tinymce.com/stable/tinymce.min.js"></script>
+
+    <script>
+        tinymce.init({
+            selector: 'textarea',
+            height: 500,
+            menubar: false,
+            plugins: [
+                'advlist autolink lists link image charmap print preview anchor',
+                'searchreplace visualblocks code fullscreen',
+                'insertdatetime media table contextmenu paste code'
+            ],
+            toolbar: 'undo redo | insert | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
+            content_css: '//www.tinymce.com/css/codepen.min.css'
+        });
+    </script>
+
     <script type="text/javascript">
         $(".multiple").select2();
         $(".multiple").select2().val({!!  json_encode(  $post->tags()->allRelatedIds()  )  !!}).trigger('change');
